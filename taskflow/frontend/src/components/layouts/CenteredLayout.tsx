7845d64052741872b450d6a32aa4f70253027d27
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-07 23:51:04 +0200

    Add CenteredLayout with GridPattern background

---
+++ taskflow/frontend/src/components/layouts/CenteredLayout.tsx
@@ -0,0 +1,15 @@
+import GridPattern from "../GridPattern";
+import type { ReactNode } from "react";
+
+type CenteredLayoutProps = {
+  children: ReactNode;
+}
+
+const CenteredLayout = ({ children }: CenteredLayoutProps ) => (
+  <div className="h-screen w-screen flex flex-col items-center justify-center text-center">
+    <GridPattern />
+    <main className="z-10 flex flex-col items-center">{children}</main>
+  </div>
+)
+
+export default CenteredLayout;
