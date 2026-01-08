52303c148b9f1a91660fd4cd06a34a445f8584a1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-08 09:12:28 +0200

    Add ErrorPage componenet to reuse for all errors

---
+++ taskflow/frontend/src/pages/errors/ErrorPage.tsx
@@ -0,0 +1,39 @@
+import Button from "../../components/ui/Button";
+import CenteredLayout from "../../components/layouts/CenteredLayout";
+import usePageTitle from "../../hooks/usePageTitle";
+import { ERRORS } from "./errors.config";
+import { FaArrowLeft } from "react-icons/fa";
+
+type ErrorCode = keyof typeof ERRORS;
+
+const ErrorPage = ({ code }: { code: ErrorCode }) => {
+  const error = ERRORS[code];
+  const Icon = error.icon
+
+  usePageTitle(error.pageTitle);
+
+  return (
+    <CenteredLayout>
+      <div className="bg-gray-200 rounded-full p-6 mb-6">
+        <Icon className="h-12 w-12 text-gray-700" />
+      </div>
+
+      <h1 className="text-4xl font-bold tracking-tight mb-2">
+        {error.title}
+      </h1>
+
+      <p className="text-muted-foreground text-lg mb-8 max-w-md">
+        {error.message}
+      </p>
+
+      <Button
+        text="Back Home"
+        color="black"
+        icon={<FaArrowLeft className="h-3 w-3" />}
+        link="/"
+      />
+    </CenteredLayout>
+  );
+};
+
+export default ErrorPage;
