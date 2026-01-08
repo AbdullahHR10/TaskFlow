5aec1282e7927874ad1cc4011d05ef2a4758efd1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-08 14:56:39 +0200

    Add authApi with signup, login, logout, and current user

---
+++ taskflow/frontend/src/api/auth.ts
@@ -0,0 +1,37 @@
+import { apiFetch } from "./client";
+import type { LoginPayLoad, SignupPayLoad } from "@/types/auth";
+import type { User } from "@/types/user";
+
+const AUTH_API_URL = "/api/v1/auth"
+
+export const authApi = {
+  signup: async (data: SignupPayLoad): Promise<void> => {
+    await apiFetch(`${AUTH_API_URL}/signup`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  },
+
+  login: async (data: LoginPayLoad): Promise<void> => {
+    await apiFetch(`${AUTH_API_URL}/login`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  },
+
+  logout: async (): Promise<void> => {
+    await apiFetch(`${AUTH_API_URL}/logout`, {
+      method: "POST",
+    });
+  },
+
+  getCurrentUser: async (): Promise<User> => {
+    const res = await apiFetch<User>(`${AUTH_API_URL}/user`);
+
+    if (!res.data) {
+      throw new Error("User not returned from API")
+    }
+
+    return res.data;
+  },
+}
