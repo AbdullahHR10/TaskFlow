d4c8cbc8592d63803b47dc0e526602f27977b5c1
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-09 20:11:25 +0200

    Add FullPageSpinner loading component

---
+++ taskflow/frontend/src/components/ui/FullPageSpinner.tsx
@@ -0,0 +1,7 @@
+const FullPageSpinner = () => (
+  <div className="min-h-screen flex items-center justify-center bg-white">
+    <div className="h-10 w-10 animate-spin rounded-full border-4 border-zinc-300 border-t-zinc-900" />
+  </div>
+)
+
+export default FullPageSpinner;
