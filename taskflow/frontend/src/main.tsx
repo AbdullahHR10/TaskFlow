f0cf325eabab9af58dd56898ed3ece8b7561d9d8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-10 19:37:12 +0200

    Use applyTheme in main

--- taskflow/frontend/src/main.tsx
+++ taskflow/frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from "@/context/AuthContext.tsx"
+import { applyTheme, getInitialTheme } from "@/theme.ts"
+
+applyTheme(getInitialTheme());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
