4ad8a73fec1cc4e2df77ee4fa610c96c5070d78d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-11 16:31:46 +0200

    Add BaseEntity and make User extend through it

---
+++ taskflow/frontend/src/types/base.ts
@@ -0,0 +1,5 @@
+export interface BaseEntity {
+  id: string;
+  created_at: string;
+  updated_at: string;
+}
--- taskflow/frontend/src/types/user.ts
+++ taskflow/frontend/src/types/user.ts
@@ -1,5 +1,6 @@
-export interface User {
-  id: string;
+import type { BaseEntity } from "./base";
+
+export interface User extends BaseEntity {
   name: string;
   email: string;
 }
