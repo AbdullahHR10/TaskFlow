82bd92ae6b6ede1729ece0d7f1702f3ab9c11908
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-11 14:05:14 +0200

    Improve routes and add main app page

--- taskflow/frontend/src/router/routes.tsx
+++ taskflow/frontend/src/router/routes.tsx
@@ -1,25 +1,41 @@
 import GuestRoute from "@/router/GuestRoute";
-
+import ProtectedRoute from "@/router/ProtectedRoute";
 import ErrorPage from "@/pages/errors/ErrorPage";
 import Login from "@/pages/auth/Login";
 import Signup from "@/pages/auth/Signup";
+import AppLayout from "@/components/layouts/AppLayout";
+import Tasks from "@/pages/app/Tasks";
+import { Navigate } from "react-router-dom";
 
 export const routes = [
   {
     path: "/login",
-    element: (
-      <GuestRoute>
-        <Login />
-      </GuestRoute>
-    ),
+    element: <GuestRoute />,
+    children: [
+      { index: true, element: <Login /> },
+    ],
   },
+
   {
     path: "/signup",
-    element: (
-      <GuestRoute>
-        <Signup />
-      </GuestRoute>
-    ),
+    element: <GuestRoute />,
+    children: [
+      { index: true, element: <Signup /> },
+    ],
+  },
+
+  {
+    path: "/app",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        element: <AppLayout />,
+        children: [
+          { index: true, element: <Navigate to="tasks" replace /> },
+          { path: "tasks", element: <Tasks /> },
+        ],
+      },
+    ],
   },
 
   { path: "/401", element: <ErrorPage code={401} /> },
