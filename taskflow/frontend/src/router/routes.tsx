1185094d6eb18bd42dba934d00d2ea62ec09df90
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2026-01-12 09:47:25 +0200

    Add Landing, PrivacyPolicy, TermsOfService and Dashbaord pages to router

--- taskflow/frontend/src/router/routes.tsx
+++ taskflow/frontend/src/router/routes.tsx
@@ -1,13 +1,22 @@
 import GuestRoute from "@/router/GuestRoute";
 import ProtectedRoute from "@/router/ProtectedRoute";
 import ErrorPage from "@/pages/errors/ErrorPage";
+import Landing from "@/pages/Landing";
 import Login from "@/pages/auth/Login";
 import Signup from "@/pages/auth/Signup";
 import AppLayout from "@/components/layouts/AppLayout";
 import Tasks from "@/pages/app/Tasks";
 import { Navigate } from "react-router-dom";
+import Dashboard from "@/pages/app/Dashboard";
+import PrivacyPolicy from "@/pages/legal/PrivacyPolicy";
+import TermsOfService from "@/pages/legal/TermsOfService";
 
 export const routes = [
+  { path: "/", element: <Landing />},
+
+  { path: "/privacy-policy", element: <PrivacyPolicy /> },
+  { path: "/terms-of-service", element: <TermsOfService /> },
+
   {
     path: "/login",
     element: <GuestRoute />,
@@ -31,7 +40,8 @@ export const routes = [
       {
         element: <AppLayout />,
         children: [
-          { index: true, element: <Navigate to="tasks" replace /> },
+          { index: true, element: <Navigate to="dashboard" replace /> },
+          { path: "dashboard", element: <Dashboard />},
           { path: "tasks", element: <Tasks /> },
         ],
       },
