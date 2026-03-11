58b565f445bd9ff82148702071fbda1131ad2a5a
AbdullahHR10 <abdullah.h.ragab1@gmail.com>
2026-03-11 21:58:33 +0200

    Adjust home href

--- taskflow/frontend/src/components/landing/Navbar.tsx
+++ taskflow/frontend/src/components/landing/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <header className="h-16 w-full flex items-center justify-between px-68 text-sm border-divider-b">
       <Logo />
       <nav className="flex gap-6 font-semibold">
-        <a href="#home">Home</a>
+        <a href="#">Home</a>
         <a href="#features">Features</a>
         <a href="#home">Developer</a>
         <a href="#home">Highlights</a>
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className="flex items-center gap-4">
         <a href="/login" className="font-semibold hover:underline">Log in</a>
         <Button text="Get Started" variant="primary" link="/signup" />
-        <ThemeToggle/>
+        <ThemeToggle />
       </div>
 
     </header>
