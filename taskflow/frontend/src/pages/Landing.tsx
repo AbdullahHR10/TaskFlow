56b2eedf7252ff006caf377aa289803f9265b34f
AbdullahHR10 <abdullah.h.ragab1@gmail.com>
2026-05-17 12:07:25 +0300

    Add Landing page

---
+++ taskflow/frontend/src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/landing/Navbar";
+import Hero from "@/components/landing/Hero";
+import Features from "@/components/landing/Features";
+import Developer from "@/components/landing/Developer";
+import Footer from "@/components/landing/Footer";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Developer />
+      <Footer />
+    </>
+  );
+}
+
+export default Landing;
